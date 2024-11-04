054ece4a40c8cef018f83d8b98bacb854113c1d4
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-11-04 18:21:02 +0530

    mobile nav links added

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -9,6 +9,7 @@ import {
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
   const { isAuthenticated, loginWithRedirect, user } = useAuth0();
@@ -30,14 +31,13 @@ const MobileNav = () => {
           ) : (
             <span>Welcome to Eats.com</span>
           )}
-          <span>welcome to eats</span>
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
+        <SheetDescription className="flex flex-col gap-4">
           {isAuthenticated ? (
             <MobileNavLinks />
           ) : (
-            <Button className="flex-1 font-bold bg-orange-500">Login</Button>
+            <Button onClick={()=> loginWithRedirect()} className="flex-1 font-bold bg-orange-500">Login</Button>
           )}
         </SheetDescription>
       </SheetContent>
