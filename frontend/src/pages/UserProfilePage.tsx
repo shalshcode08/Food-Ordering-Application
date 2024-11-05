07c85d335e39ba2db4d6c1634444982f1499d256
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-11-05 12:34:39 +0530

    User profile form

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,9 @@
+import UserProfileForm from "@/forms/user-profile-form/UserProfileFrom"
+
+const UserProfilePage = () => {
+  return (
+    <UserProfileForm />
+  )
+}
+
+export default UserProfilePage
